a17d633a744663a90d86b5500b6f2880639f0a91
ShivanshRai16 <110758483+ShivanshRai16@users.noreply.github.com>
2022-08-07 12:05:26 +0530

    Create 1-Percentage-Calculator.java

---
+++ Projects/1-Percentage-Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Percentage Calculator.");
+        Scanner mark = new Scanner(System.in);
+        System.out.println("Enter the marks in first subject : ");
+        float mark1 = mark.nextFloat();
+        System.out.println("Enter the marks in second subject : ");
+        float mark2 = mark.nextFloat();
+        System.out.println("Enter the marks in third subject : ");
+        float mark3 = mark.nextFloat();
+        System.out.println("Enter the marks in fourth subject : ");
+        float mark4 = mark.nextFloat();
+        System.out.println("Enter the marks in fifth subject : ");
+        float mark5 = mark.nextFloat();
+        float sum = mark1 + mark2 + mark3 + mark4 + mark5;
+        float div = sum / 500;
+        float per = div * 100;
+        System.out.println("Percetage is : ");
+        System.out.println(per);
+    }
+}
