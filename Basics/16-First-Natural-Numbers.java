abf1b19f8f742fc9d3267e7051757a119d7794f0
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-09 23:26:11 +0530

    Update 16-First-Natural-Numbers.java

--- Basics/16-First-Natural-Numbers.java
+++ Basics/16-First-Natural-Numbers.java
@@ -1,5 +1,6 @@
 public class Practice2 {
     public static void main(String[] args) {
+        //Using while Loop
         int i = 1;
         while (i<=100){
             System.out.println(i);
@@ -12,5 +13,10 @@ public class Practice2 {
             System.out.println(a);
             a++;
         } while (a<5);
+        
+        // Using for loop
+        for (int i=0; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
