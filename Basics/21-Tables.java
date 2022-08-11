eba8676c6cc3d134c0d7a487b428ba59795412ed
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-11 21:19:23 +0530

    Create 21-Tables.java

---
+++ Basics/21-Tables.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the to print that table : ");
+        int num = sc.nextInt();
+
+        System.out.println("Table is : ");
+        for (int i=1; i<=10; i++){
+            int mul = num * i;
+            System.out.print(num); 
+            System.out.print(" x ");
+            System.out.print(i);
+            System.out.print(" = ");
+            System.out.println(mul);
+        }
+    }
+}
