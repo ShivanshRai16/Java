93fdb7a354b43c4ec5b79d0601722d245c770934
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-11 20:58:41 +0530

    Create 20-Sum-Natural-Number.java

---
+++ Basics/20-Sum-Natural-Number.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number for sum : ");
+        int num = sc.nextInt();
+
+        // Sum of n natural number.
+        System.out.println("Numbers are : ");
+        int i=0;
+        int sum = 0;
+        while(i<=num){
+            System.out.println(i);
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("Sum of the n number is : ");
+        System.out.println(sum);
+      
+        // Sum of n even number.
+        while(i<=num){
+            System.out.println(i);
+            sum = sum + i;
+            i = i+2;
+        }
+        System.out.println("Sum of the n even number is : ");
+        System.out.println(sum);
+    }
+}
