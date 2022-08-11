8d86f2809e9e68600fcf9cfd9e4ad1f962e1391b
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-11 16:36:25 +0530

    Create 19-Star-Pattern.java

---
+++ Basics/19-Star-Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+
+        // * * *
+        // * *
+        // *
+        for (int i=num; i>0; i--){
+            for (int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
