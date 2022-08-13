43d572f824b54c310f9c557c94ed64a33c98a3d0
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-14 00:54:43 +0530

    Create 02. Km to Miles.java

---
+++ Basics/02. Km to Miles.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Kilometer : ");
+        float km = sc.nextFloat();
+
+        float miles = (float) (km * 0.6213);
+        System.out.println("The miles is : ");
+        System.out.println(miles);
+    }
+}
