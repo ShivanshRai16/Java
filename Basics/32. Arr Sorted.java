50ba6f3ab151ba53286649ea6b93e51c45691475
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-14 23:57:48 +0530

    Update 32. Arr Sorted.java

--- Basics/32. Arr Sorted.java
+++ Basics/32. Arr Sorted.java
@@ -34,3 +34,21 @@ public class Practice2 {
         }
     }
 }
+
+
+/*
+        boolean isSorted = true;
+        int [] arr = {1, 12, 3, 4, 5, 34, 67};
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The Array is sorted");
+        }
+        else{
+            System.out.println("The Array is not sorted");
+        }
+*/
