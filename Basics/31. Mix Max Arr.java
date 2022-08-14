4fc1975e751821ec983ea19450f3c0265a827319
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-14 23:57:16 +0530

    Update 31. Mix Max Arr.java

--- Basics/31. Mix Max Arr.java
+++ Basics/31. Mix Max Arr.java
@@ -37,3 +37,19 @@ public class Practice2 {
         System.out.println(max);
     }
 }
+
+
+/*
+        int [] arr = {1, 2100, 3, 455, 5, 34, 67};
+        int max = Integer.MIN_VALUE;
+        for(int e: arr){
+            if(e>max){
+                max = e;
+            }
+        }
+        System.out.println("the value of the maximum element in this array is: "+ max);
+
+        // Practice Problem 6
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+*/
