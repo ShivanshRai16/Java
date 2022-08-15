85fd1b826b243a1c649658a991ff0d60fd8a7835
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-15 11:53:56 +0530

    Update 33. Method.java

--- Basics/33. Method.java
+++ Basics/33. Method.java
@@ -1,4 +1,6 @@
 public class Practice2 {
+//  Method invocation using object creation.
+//  int logic(int x, int y){
     static int logic(int x, int y){
         int z;
         if(x>y){
@@ -14,12 +16,15 @@ public class Practice2 {
         int a = 5;
         int b = 6;
         int c;
+//      Practice2 obj = new Practice2();
+//      c = obj.logic(a, b);
         c = logic(a, b);
         System.out.println(c);
 
         int a1 = 3;
         int b1 = 1;
         int c1;
+//      c1 = obj.logic(a1, b1);
         c1 = logic(a1, b1);
         System.out.println(c1); 
     }
