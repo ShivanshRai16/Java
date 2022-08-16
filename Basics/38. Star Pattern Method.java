b81fec82dac2604a67b2bc9e98190ee238d68e72
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-16 19:32:22 +0530

    Update 38. Star Pattern Method.java

--- Basics/38. Star Pattern Method.java
+++ Basics/38. Star Pattern Method.java
@@ -66,3 +66,49 @@ Enter the number of star :
 * * * *
 * * * * *
 */
+
+
+import java.util.Scanner;
+
+public class Practice2 {
+    static int pattern(int x){
+        for (int i=0; i<x; i++){   
+            for (int j=0; j<=i; j++){ 
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for (int i=x; i>0; i--){   
+            for (int j=0; j<i; j++){ 
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of star : ");
+        int st = sc.nextInt();
+        
+        int star;
+        star = pattern(st);
+    }
+}
+
+/*
+Enter the number of star : 
+5
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
