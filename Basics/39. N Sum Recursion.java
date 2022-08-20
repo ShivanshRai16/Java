89ef20c56603809107fb118e232f08c2a3344ae4
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-20 23:36:25 +0530

    Update 39. N Sum Recursion.java

--- Basics/39. N Sum Recursion.java
+++ Basics/39. N Sum Recursion.java
@@ -1,3 +1,4 @@
+// Recursion Method
 import java.util.Scanner;
 
 // sum(n) = 1 + 2 + 3... + n
@@ -15,6 +16,29 @@ public class Practice2 {
         else{
             return x + sum(x - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the n term for sum : ");
+        int sum = sc.nextInt();
+
+        System.out.println("The sum is : " + sum(sum));
+    }
+}
+
+
+// Iteration Method
+import java.util.Scanner;
+
+public class Practice2 {
+    static int sum(int x){
+        int rec = 0;
+        for (int i=0; i<x; i++){
+            rec = x + sum(x-1);
+        }
+        return rec;
     }
 
     public static void main(String[] args) {
