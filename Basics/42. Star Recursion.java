a53411c499285f84e4b21f036cbba6cdba70edd6
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-20 17:19:37 +0530

    Update 42. Star Recursion.java

--- Basics/42. Star Recursion.java
+++ Basics/42. Star Recursion.java
@@ -36,3 +36,43 @@ The patten is :
 * * * *
 * * * * *
 */
+
+import java.util.Scanner;
+
+public class Practice2 {  
+    static void Pattern(int num){
+        if (num > 0){
+            // Pattern(num - 1);
+            for (int i=num; i>0; i--){
+                System.out.print("* ");
+            }
+            System.out.println();
+            Pattern(num - 1);
+        }
+    }
+    // pattern1_rec(3)
+    // pattern1_rec(2) + 3 times star and new line
+    // pattern1_rec(1) + 2 times star and new line + 3 times star and new line
+    // pattern1_rec(0) + 1 times star and new line + 2 times star and new line + 3 times star and new line
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number to print its star : ");
+        int n = sc.nextInt();
+
+        System.out.println("The patten is : ");
+        Pattern(n);
+    }
+}
+
+/*
+Enter the number to print its star : 
+5
+The patten is : 
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
