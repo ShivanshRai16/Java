6cb80e701822804f8c586f52df4a0927ccd0f6d6
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-23 19:31:10 +0530

    Update 48. Constructors.java

--- Basics/48. Constructors.java
+++ Basics/48. Constructors.java
@@ -40,3 +40,60 @@ public class Practice2{
         System.out.println(harry.getId());
     }
 }
+
+// Constructors Overloading
+    class Employee{
+        private int id;
+        private String name;
+
+        public Employee(){
+            id = 45;
+            name = "Harry";
+        }
+
+        // Constructer
+        public Employee(String myname, int myid){
+            id = myid;
+            name = myname;
+        }
+
+        public Employee(String myname){
+            id = 4;
+            name = myname;
+        }
+
+        // Getters
+        public String getName(){
+            return name;
+        }
+        
+        // Setters
+        public void setName(String n){
+            this.name = n;
+            // name = n;
+        }
+
+        public int getId(){
+            return id;
+        }
+
+        public void setId(int i){
+            id = i;
+            // this.id = i;
+        }
+    }
+
+    public class Practice2{
+        public static void main(String[] args) {
+            // Employee harry = new Employee("Harry s singh", 455);
+            Employee harry = new Employee("Harry r singh");
+            // Employee harry = new Employee();
+
+            // harry.setName("Harry Singh");
+            // harry.setId(45);
+
+            System.out.println(harry.getName());
+            System.out.println(harry.getId());
+        }
+    }
+
