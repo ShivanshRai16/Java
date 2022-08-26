d4ad1a7ca58c280cf31f3c3b55f643ae3275e5ce
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-26 23:29:45 +0530

    Update 03. Sum.java

--- Q Practice/Array/03. Sum.java
+++ Q Practice/Array/03. Sum.java
@@ -1,16 +1,11 @@
 public class Practice3{
-    static void sort(int [] arr){
-        int sum = arr[0];
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 10};
+        int sum = 0;
 
-        for (int i:arr){
+        for(int i=0; i<arr.length; i++){
             sum = sum + arr[i];
         }
-        System.out.println("The sum is : " + sum);
-
-    }
-
-    public static void main(String[] args) {
-        int str[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        sort(str);
+        System.out.println(sum);
     }
 }
