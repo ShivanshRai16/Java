7bae0c5c5c9c7afeb6e03130890c31f8bef8cb58
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-26 23:39:39 +0530

    Update 03. Sum.java

--- Q Practice/Array/03. Sum.java
+++ Q Practice/Array/03. Sum.java
@@ -9,3 +9,22 @@ public class Practice3{
         System.out.println(sum);
     }
 }
+
+
+// Sum and Average
+public class Practice3{
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 10};
+        int sum = 0;
+
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+        
+        int avg = sum/arr.length;
+
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+}
+
