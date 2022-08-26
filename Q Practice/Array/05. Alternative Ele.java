93b5010a56418709aa2b513a0373d7ec87060fb3
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-26 19:54:23 +0530

    Create 05. Alternative Ele.java

---
+++ Q Practice/Array/05. Alternative Ele.java
@@ -0,0 +1,12 @@
+public class Practice3{
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int skip;
+
+        for (int i=0; i<arr.length; i=i+2){
+            skip = arr[i]; 
+
+            System.out.println(skip);
+        }
+    }
+}
