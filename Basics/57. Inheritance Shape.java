0a87695281964dd8f1b41f80dd03bd8ec62cc1c8
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-27 23:53:34 +0530

    Create 57. Inheritance Shape.java

---
+++ Basics/57. Inheritance Shape.java
@@ -0,0 +1,35 @@
+class Circle{
+    int r;
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+}
+
+class Cyli extends Circle{
+    int h;
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+}
+
+public class Practice2{
+    public static void main(String[] args) {
+        Circle radius = new Circle();
+        radius.setR(5);
+        System.out.println("The radius of the circle is : " + radius.getR());
+
+        Cyli height = new Cyli();
+        height.setH(10);
+        System.out.println("The height of the cylinder is : " + height.getH());
+    }
+}
