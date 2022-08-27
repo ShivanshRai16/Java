0ba32c68ee2cc0d876dd2831758ae3e09a295155
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-27 16:37:21 +0530

    Update 54. this and super keyword.java

--- Basics/54. this and super keyword.java
+++ Basics/54. this and super keyword.java
@@ -1,3 +1,4 @@
+// This
 class test{
     public int a;
 
@@ -21,6 +22,43 @@ class test{
 public class Practice2{
     public static void main(String[] args) {
         test t = new test(5);
+        System.out.println(t.getA());
+    }
+}
+
+
+// Super
+class test{
+    public int a;
+
+    // test(int v){
+    //     a = v;
+    // }
+    
+    test(int a){
+        this.a = a; // this variable name is same inside method so this is used to reffer it is basically a referance.
+    }    
+
+    public int returnone(){
+        return 1;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
+
+class test2 extends test{
+    test2(int c){
+        super(c); // referance variable used to refer immediate parent class.
+        System.out.println("I am an constructor.");
+    }
+}
+
+public class Practice2{
+    public static void main(String[] args) {
+        test t = new test(5);
+        test2 t2 = new test2(65);
         System.out.println(t.getA());
     }
 }
