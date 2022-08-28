45a5fefb3d55d0c312b01548e436bb1761dcc802
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-28 17:16:28 +0530

    Create 03. Int or not.java

---
+++ Basics/03. Int or not.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Practice2{
+    public static void main(String[] args) {
+        System.out.println("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
