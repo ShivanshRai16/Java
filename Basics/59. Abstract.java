dc601d1d8fd9be1f41b936f13727919ca8fdeb9e
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-28 16:38:28 +0530

    Create 59. Abstract.java

---
+++ Basics/59. Abstract.java
@@ -0,0 +1,45 @@
+abstract class Parent{ // Using this class other class will be formed.
+                       // abstract's object cannot be formed. 
+                       // a standard is formed and used as per own.
+                       
+    // Constructor
+    public Parent(){
+        System.out.println("I am the constructor of base.");
+    }
+
+    // Method
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+
+    // Abstract
+    abstract public void greet(); // Declare things as per your need.
+    abstract public void greet1();
+
+}
+
+class Child extends Parent{
+    @Override
+    public void greet(){ // greet method overriden.
+        System.out.println("Good Morning.");
+    }
+    @Override
+    public void greet1(){ // greet method overriden.
+        System.out.println("Good Afternoon.");
+    }
+}
+
+abstract class Child2 extends Parent{
+    public void th(){
+        System.out.println("This is to check.");
+    }
+}
+
+public class Practice2{
+    public static void main(String[] args) {
+        // Parent p = new Parent();  // Error because abstract class.
+        // Child2 c = new Child2();  // same error
+        Child c = new Child();
+
+    }
+}
