25a8bce8ed90be9e723c1df007dbeca62acbd3ff
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-08-28 18:17:52 +0530

    Create 04. Encrypt Decrypt.java

---
+++ Basics/04. Encrypt Decrypt.java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+
+public class Practice2{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+        // Decrypting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
