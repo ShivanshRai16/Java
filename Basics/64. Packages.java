7071aec3883a6e4ff909da2266815381bc175d22
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-09-03 10:10:01 +0530

    Create 64. Packages.java

---
+++ Basics/64. Packages.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//import java.util.*;
+
+public class Practice2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("This is my scanner taking input as " + a);
+    }
+}
+
+// javac -d . * . java 
+/* By puting the above command if at the top 
+ * there is a package then its class folder will
+ * be created and that package command should be 
+ * there in all the code so that they could be 
+ * in the same package class folder.
+ * Make sure to write the package corectly.
+ * This will creatre class folder which can
+ * be used by other OS.
+*/
