1b97531da6b9d8185bab665ce6031f14115f0185
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-09-07 21:50:19 +0530

    Update 74. Exception Class.java

--- Basics/74. Exception Class.java
+++ Basics/74. Exception Class.java
@@ -63,3 +63,38 @@ public class Practice2{
         }
     }
 }
+
+
+// Exception example
+import java.util.Scanner;
+
+public class Practice2{
+    public static void main(String[] args) {
+        boolean flag = true;
+        int [] num = new int[3];
+        num[0] = 5;
+        num[1] = 10;
+        num[2] = 15;
+
+        Scanner sc = new Scanner(System.in);
+        
+        int index;
+        int i=0;
+
+        while(flag && i<5){
+            try{
+                System.out.println("Enter the index value : ");
+                index = sc.nextInt();
+                System.out.println("The value of marks[index] is " + num[index]);
+                break;
+            }
+            catch(Exception e){
+                System.out.println("Invalid Index");
+                i++;
+            }
+        }
+        if(i>=5){
+            System.out.println("Error");
+        }
+    }
+}
